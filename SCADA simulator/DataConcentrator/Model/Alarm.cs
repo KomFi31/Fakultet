82d1dcfd2f6d542f095a8b493f8be8d7e6e65d4b
Filip Komarica <komfilo031@gmail.com>
2026-09-01 01:15:01 +0200

    Added Alarm class

--- SCADA simulator/DataConcentrator/Model/Alarm.cs
+++ SCADA simulator/DataConcentrator/Model/Alarm.cs
@@ -1,6 +1,98 @@
-﻿namespace DataConcentrator.Model
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace DataConcentrator.Model
 {
-    internal class Alarm
+    // Above - aktivacija pri prekoracenju granice, Below - aktivacija prilikom pada ispod granice
+    public enum AlarmCondition
     {
+        Above,
+        Below
+    }
+
+    public enum AlarmState
+    {
+        Inactive,
+        Active,
+        Acknowledged
+    }
+
+    public class Alarm : INotifyPropertyChanged
+    {
+        private int id;
+        private string tagName;
+        private double limit;
+        private AlarmCondition condition;
+        private string message;
+        private AlarmState state;
+
+        [Key]
+        public int Id
+        {
+            get { return id; }
+            set
+            {
+                id = value;
+                OnPropertyChanged("Id");
+            }
+        }
+
+        // Ime AnalogInput taga za koji je alarm vezan
+        public string TagName
+        {
+            get { return tagName; }
+            set
+            {
+                tagName = value;
+                OnPropertyChanged("TagName");
+            }
+        }
+
+        public double Limit
+        {
+            get { return limit; }
+            set
+            {
+                limit = value;
+                OnPropertyChanged("Limit");
+            }
+        }
+
+        public AlarmCondition Condition
+        {
+            get { return condition; }
+            set
+            {
+                condition = value;
+                OnPropertyChanged("Condition");
+            }
+        }
+
+        public string Message
+        {
+            get { return message; }
+            set
+            {
+                message = value;
+                OnPropertyChanged("Message");
+            }
+        }
+
+        public AlarmState State
+        {
+            get { return state; }
+            set
+            {
+                state = value;
+                OnPropertyChanged("State");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged(string property)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(property));
+        }
     }
 }
