3c9b14facf9a80a63f1574651ec12a4f8517f9fa
Filip Komarica <komfilo031@gmail.com>
2026-09-01 23:50:43 +0200

    Finished Data concentrator manager for tags and alarms

--- SCADA simulator/DataConcentrator/DataConcentratorManager.cs
+++ SCADA simulator/DataConcentrator/DataConcentratorManager.cs
@@ -30,7 +30,7 @@ namespace DataConcentrator
         private DataConcentratorManager()
         {
         }
-
+        #region Tag Management
         public List<Tag> GetAllTags()
         {
             return ContextClass.Instance.Tags.ToList();
@@ -63,10 +63,85 @@ namespace DataConcentrator
             if (tag == null)
                 return false;
 
+            // Brisanjem taga uklanjaju se i alarmi vezani za njega.
+            List<Alarm> tagAlarms = GetAlarmsForTag(name);
+
+            ContextClass.Instance.Alarms.RemoveRange(tagAlarms);
             ContextClass.Instance.Tags.Remove(tag);
+
             ContextClass.Instance.SaveChanges();
 
             return true;
         }
+
+        #endregion
+
+        #region Alarm Management
+
+        public List<Alarm> GetAllAlarms()
+        {
+            return ContextClass.Instance.Alarms.ToList();
+        }
+
+        public Alarm GetAlarm(int id)
+        {
+            return ContextClass.Instance.Alarms
+                .FirstOrDefault(alarm => alarm.Id == id);
+        }
+
+        public List<Alarm> GetAlarmsForTag(string tagName)
+        {
+            return ContextClass.Instance.Alarms
+                .Where(alarm => alarm.TagName == tagName)
+                .ToList();
+        }
+
+        public bool AddAlarm(Alarm alarm)
+        {
+            if (alarm == null)
+                return false;
+
+            // Alarm moze biti vezan iskljucivo za analogni ulazni tag.
+            Tag tag = GetTag(alarm.TagName);
+
+            if (!(tag is AnalogInput))
+                return false;
+
+            alarm.State = AlarmState.Inactive;
+
+            ContextClass.Instance.Alarms.Add(alarm);
+            ContextClass.Instance.SaveChanges();
+
+            return true;
+        }
+
+        public bool RemoveAlarm(int id)
+        {
+            Alarm alarm = GetAlarm(id);
+
+            if (alarm == null)
+                return false;
+
+            ContextClass.Instance.Alarms.Remove(alarm);
+            ContextClass.Instance.SaveChanges();
+
+            return true;
+        }
+
+        public bool AcknowledgeAlarm(int id)
+        {
+            Alarm alarm = GetAlarm(id);
+
+            if (alarm == null || alarm.State != AlarmState.Active)
+                return false;
+
+            alarm.State = AlarmState.Acknowledged;
+            ContextClass.Instance.SaveChanges();
+
+            return true;
+        }
+
+        #endregion
+
     }
 }
