add2942245e5b4eb083d38b373f9f1a176d74b63
Filip Komarica <komfilo031@gmail.com>
2026-09-01 02:53:07 +0200

    Added ActivatedAlarm class

--- SCADA simulator/DataConcentrator/Model/ActivatedAlarm.cs
+++ SCADA simulator/DataConcentrator/Model/ActivatedAlarm.cs
@@ -1,6 +1,24 @@
-﻿namespace DataConcentrator.Model
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace DataConcentrator.Model
 {
-    internal class ActivatedAlarm
+    public class ActivatedAlarm // Klasa prakticno vraca povratnu informaciju o alarmu koji se aktivira detaljno
     {
+        [Key]
+
+        // Ne treba pomesati sa AlarmID koji predstavlja ID alarma koji se aktivirao
+        // Dok ActivatedAlarmId predstavlja ID notifikacije za aktiviran alarm
+        public int ActivatedAlarmId { get; set; }
+                                                  
+
+        // ID alarma koji se aktivirao
+        public int AlarmId { get; set; }
+
+        public string TagName { get; set; }
+
+        public string Message { get; set; }
+
+        public DateTime TimeStamp { get; set; }
     }
 }
