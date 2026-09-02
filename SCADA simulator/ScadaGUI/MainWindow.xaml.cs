b40523d310129bec1c29af8e7462cd3470eb8aef
Filip Komarica <komfilo031@gmail.com>
2026-09-02 12:01:43 +0200

    Initial testing of CRUD tag operations

--- SCADA simulator/ScadaGUI/MainWindow.xaml.cs
+++ SCADA simulator/ScadaGUI/MainWindow.xaml.cs
@@ -1,20 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Data.Entity;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using System.Windows.Threading;
 using DataConcentrator;
+using DataConcentrator.Model;
 using PLCSimulator;
 
 namespace ScadaGUI
@@ -24,53 +12,98 @@ namespace ScadaGUI
     /// </summary>
     public partial class MainWindow : Window
     {
-    public MainWindow()
+        public MainWindow()
         {
             InitializeComponent();
 
-            //foreach (AnalogInput ai in ...)
-            //{
-            //    ai.AlarmActivated += OnAlarmActivated;
-            //    ai.StartScan();
-            //}
+            try
+            {
+                DataConcentratorManager manager =
+                    DataConcentratorManager.Instance;
 
+                // Cistimo test podatke ako su ostali od prethodnog pokretanja.
+                manager.RemoveTag("TEST_AI");
+
+                AnalogInput testTag = new AnalogInput
+                {
+                    Name = "TEST_AI",
+                    Description = "Initial description",
+                    IOAddress = "ADDR001",
+                    ScanTime = 1,
+                    OnScan = true,
+                    LowLimit = 0,
+                    HighLimit = 100,
+                    Units = "C",
+                    Deadband = 1,
+                    Hysteresis = 2
+                };
+
+                bool added = manager.AddTag(testTag);
+
+                AnalogInput updatedTag = new AnalogInput
+                {
+                    Name = "TEST_AI",
+                    Description = "Updated description",
+                    IOAddress = "ADDR001",
+                    ScanTime = 0.5,
+                    OnScan = true,
+                    LowLimit = 0,
+                    HighLimit = 120,
+                    Units = "C",
+                    Deadband = 0.5,
+                    Hysteresis = 3
+                };
+
+                bool updated = manager.UpdateTag(updatedTag);
+
+                AnalogInput loadedTag =
+                    manager.GetTag("TEST_AI") as AnalogInput;
+
+                Alarm testAlarm = new Alarm
+                {
+                    TagName = "TEST_AI",
+                    Limit = 80,
+                    Condition = AlarmCondition.Above,
+                    Message = "Test temperature alarm"
+                };
+
+                bool alarmAdded = manager.AddAlarm(testAlarm);
+
+                int alarmsBeforeDelete =
+                    manager.GetAlarmsForTag("TEST_AI").Count;
+
+                bool removed = manager.RemoveTag("TEST_AI");
+
+                Tag tagAfterDelete =
+                    manager.GetTag("TEST_AI");
+
+                int alarmsAfterDelete =
+                    manager.GetAlarmsForTag("TEST_AI").Count;
+
+                MessageBox.Show(
+                    "Tag added: " + added +
+                    "\nTag updated: " + updated +
+                    "\nDescription: " + loadedTag?.Description +
+                    "\nScan time: " + loadedTag?.ScanTime +
+                    "\nAlarm added: " + alarmAdded +
+                    "\nAlarms before delete: " + alarmsBeforeDelete +
+                    "\nTag removed: " + removed +
+                    "\nTag exists after delete: " + (tagAfterDelete != null) +
+                    "\nAlarms after delete: " + alarmsAfterDelete
+                );
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
         }
 
-
-        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        private void Window_Closing(
+            object sender,
+            System.ComponentModel.CancelEventArgs e)
         {
-        //    //abort input threads
-        //    foreach(AnalogInput ai in ContextClass.Instance.AnalogInputs)
-        //    {
-        //        ai.StopScan();
-        //    }
-        //    foreach(DigitalInput di in ContextClass.Instance.DigitalInputs)
-        //    {
-        //        di.StopScan();
-        //    }
-
-        //    //abort simulator threads
-        //    if (PLC.Instance != null)
-        //    {
-        //        PLC.Instance.Abort();
-        //    }
-
-        //    ContextClass.Instance.SaveChanges();
-        //    ContextClass.Instance.Dispose();
+            // Za sada ostavljamo handler jer je povezan sa MainWindow.xaml.
+            // Kasnije cemo ovde zaustaviti scanning i PLC simulator.
         }
-
-        //static void OnAlarmActivated(string alarmName)
-        //{
-        //    Application.Current.Dispatcher.BeginInvoke(
-        //    DispatcherPriority.Background,
-        //        new Action(() =>
-        //        {
-        //            ActivatedAlarm alarm = new ActivatedAlarm(ContextClass.Instance.Alarms.Find(alarmName));
-        //            ContextClass.Instance.ActivatedAlarms.Add(alarm);
-        //            ContextClass.Instance.SaveChanges();
-        //        }));
-
-        //}
-
     }
 }
